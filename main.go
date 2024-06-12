c78f867288a085abc2c7aebf4e5fe0f92271af35
TianyueLi1227 <tianyueli@riema.xyz>
2024-06-12 19:20:33 +0800

    save module

--- main.go
+++ main.go
@@ -2,53 +2,34 @@ package main
 
 import (
 	"github.com/nsf/termbox-go"
+	"github.com/tianyueli8227/terminal-chat/anime"
 )
 
-func drawText(x, y int, text string, fg, bg termbox.Attribute) {
-	for i, c := range text {
-		termbox.SetCell(x+i, y, c, fg, bg)
-	}
-}
-
-func main() {
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
-
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	termbox.Flush()
-
+func handleInput() {
 	input := ""
 	inputStartX, inputStartY := 10, 5
 	maxLines := 10
 	lines := []string{}
 
 	for {
-		// Draw input box and the current input text
 		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-		drawText(inputStartX, inputStartY, "Input: "+input, termbox.ColorGreen, termbox.ColorDefault)
+		anime.drawText(inputStartX, inputStartY, "Input: "+input, termbox.ColorGreen, termbox.ColorDefault)
 
-		// Draw the last maxLines lines of output
 		start := 0
 		if len(lines) > maxLines {
 			start = len(lines) - maxLines
 		}
 		for i, line := range lines[start:] {
-			drawText(inputStartX, inputStartY+2+i, line, termbox.ColorBlue, termbox.ColorDefault)
+			anime.drawText(inputStartX, inputStartY+2+i, line, termbox.ColorBlue, termbox.ColorDefault)
 		}
 
 		termbox.Flush()
 
-		// Wait for an event
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
 			if ev.Key == termbox.KeyEsc {
-				// Exit on ESC key
 				return
 			} else if ev.Key == termbox.KeyEnter {
-				// Add input text to lines and clear input
 				lines = append(lines, input)
 				input = ""
 			} else if ev.Key == termbox.KeyBackspace || ev.Key == termbox.KeyBackspace2 {
@@ -62,12 +43,71 @@ func main() {
 			panic(ev.Err)
 		}
 
-		// Remove old lines if necessary
 		if len(lines) > maxLines {
 			lines = lines[len(lines)-maxLines:]
 		}
 
-		// Make sure the terminal is updated
 		termbox.Flush()
 	}
+}
+
+func displayMenu() {
+	options := []string{"Option 1", "Option 2", "Option 3"}
+	selected := 0
+
+	for {
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+		for i, option := range options {
+			fg := termbox.ColorWhite
+			if i == selected {
+				fg = termbox.ColorRed
+			}
+			anime.drawText(10, 5+i, option, fg, termbox.ColorDefault)
+		}
+
+		termbox.Flush()
+
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyEsc {
+				return
+			} else if ev.Key == termbox.KeyArrowUp {
+				if selected > 0 {
+					selected--
+				}
+			} else if ev.Key == termbox.KeyArrowDown {
+				if selected < len(options)-1 {
+					selected++
+				}
+			} else if ev.Key == termbox.KeyEnter {
+				handleOption(selected)
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+	}
+}
+
+func handleOption(option int) {
+	switch option {
+	case 0:
+		handleInput()
+	case 1:
+		// Add more functionality here for Option 2
+		handleInput()
+	case 2:
+		// Add more functionality here for Option 3
+		handleInput()
+	}
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	displayMenu()
 }
